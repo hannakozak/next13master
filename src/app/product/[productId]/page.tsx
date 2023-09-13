edb8144765df052922a30d4a7e82b0e9bacbb08e
hannawkozak <hannawkozak@gmail.com>
2023-09-13 23:21:08 +0100

    feat: add dynamic metadata to single product page

--- src/app/product/[productId]/page.tsx
+++ src/app/product/[productId]/page.tsx
@@ -1,5 +1,23 @@
+import { type Metadata } from "next";
 import { getProductById } from "@/api/products";
 import { Product } from "@/ui/organisms/Product";
+
+export const generateMetadata = async ({
+	params,
+}: {
+	params: { productId: string };
+}): Promise<Metadata> => {
+	const product = await getProductById(params.productId);
+	return {
+		title: `${product.name} - Next.js Shop`,
+		description: product.description,
+		openGraph: {
+			title: `${product.name} - Next.js Shop`,
+			description: product.description,
+			images: [product.coverImage.src],
+		},
+	};
+};
 
 export default async function SingleProductPage({
 	params,
