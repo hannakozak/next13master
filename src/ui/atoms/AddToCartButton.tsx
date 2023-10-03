6f296014e4d2c0553f5603826c540394df82f937
hannawkozak <hannawkozak@gmail.com>
2023-10-03 13:25:22 +0100

    feat: add add to cart button

---
+++ src/ui/atoms/AddToCartButton.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { experimental_useFormStatus as useFormStatus } from "react-dom";
+
+export const AddToCartButton = () => {
+	const formStatus = useFormStatus();
+	formStatus.pending;
+	return (
+		<button
+			type="submit"
+			disabled={formStatus.pending}
+			className="brighness-100 font-semiboldbold h-14 w-full rounded-md bg-gradient-to-r from-blue-500  via-blue-800 to-blue-950 text-xl text-white shadow-md hover:brightness-125 disabled:cursor-wait disabled:brightness-50"
+		>
+			Add To Card
+		</button>
+	);
+};
