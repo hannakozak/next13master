6c0d1ff6150220e515562ff5b9dfa8bc1e765a67
hannawkozak <hannawkozak@gmail.com>
2023-10-10 22:42:32 +0100

    refactor: general code refactor

--- src/ui/molecules/Navigation.tsx
+++ src/ui/molecules/Navigation.tsx
@@ -1,9 +1,6 @@
 import { type UrlObject } from "url";
 import { type Route } from "next";
 import { ActiveLink } from "../atoms/ActiveLink";
-import Link from "next/link";
-import { ShoppingCart } from "lucide-react";
-import { getCartFromCookies } from "@/api/cart";
 
 type NavLinksProps = {
 	href: Route<string> | UrlObject;
@@ -23,37 +20,24 @@ const navLinks: NavLinksProps[] = [
 	},
 ];
 export const Navigation = async () => {
-	const cart = await getCartFromCookies();
-	const totalQuantity = cart?.orderItems.reduce(
-		(sum, item) => sum + item.quantity,
-		0,
-	);
 	return (
-		<nav>
-			<ul className="flex justify-center">
-				{navLinks.map(({ href, label, exact }) => (
-					<li key={label} className="px-2">
-						<ActiveLink
-							href={href}
-							exact={exact}
-							className="font-semibold text-blue-600 hover:text-blue-800"
-							activeClassName="text-blue-800 border border-2 border-b-blue-800"
-						>
-							{label}
-						</ActiveLink>
-					</li>
-				))}
-				<Link
-					href={"/cart"}
-					className="group -m-2 flex items-center p-2"
-				>
-					<ShoppingCart />
-					<span className="ml-2 text-sm font-medium">
-						{totalQuantity}
-					</span>
-					<span className="sr-only">Items in cart, view bag</span>
-				</Link>
-			</ul>
-		</nav>
+		<>
+			<nav>
+				<ul className="flex justify-center">
+					{navLinks.map(({ href, label, exact }) => (
+						<li key={label} className="px-2">
+							<ActiveLink
+								href={href}
+								exact={exact}
+								className="font-semibold text-blue-600 hover:text-blue-800"
+								activeClassName="text-blue-800 border border-2 border-b-blue-800"
+							>
+								{label}
+							</ActiveLink>
+						</li>
+					))}
+				</ul>
+			</nav>
+		</>
 	);
 };
--- src/ui/organisms/Header.tsx
+++ src/ui/organisms/Header.tsx
@@ -2,13 +2,31 @@ import { Suspense } from "react";
 import { Navigation } from "../molecules/Navigation";
 import { SearchInput } from "../molecules/SearchInput";
 import { revalidatePath } from "next/cache";
+import { getCartFromCookies } from "@/api/cart";
+import { ShoppingCart } from "lucide-react";
+import { ActiveLink } from "../atoms/ActiveLink";
 
 export const Header = async () => {
 	revalidatePath("/cart");
+	const cart = await getCartFromCookies();
+	const totalQuantity = cart?.orderItems.reduce(
+		(sum, item) => sum + item.quantity,
+		0,
+	);
 	return (
 		<header className="fixed top-0 flex w-screen items-center justify-between  border-b-2 bg-white px-6 py-3">
 			<Navigation />
 			<aside className="flex items-center gap-3">
+				<ActiveLink
+					href={"/cart"}
+					className="group -m-2 flex items-center p-2"
+				>
+					<ShoppingCart />
+					<span className="ml-2 text-sm font-medium">
+						{totalQuantity}
+					</span>
+					<span className="sr-only">Items in cart, view bag</span>
+				</ActiveLink>
 				<Suspense>
 					<SearchInput />
 				</Suspense>
